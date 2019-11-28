1c8b410b94739eb99f4c24f645fdcb5f3dccac61
HelenCVM <52894597+HelenCVM@users.noreply.github.com>
2019-11-28 12:41:44 -0500

    ModificacionCreacionUsuario

--- SistemaDeGestion/config/creacion_usuario.php
+++ SistemaDeGestion/config/creacion_usuario.php
@@ -14,13 +14,14 @@ color: red;
 //incluir conexión a la base de datos
 include 'conexionBD.php';
 $cedula = isset($_POST["cedula"]) ? trim($_POST["cedula"]) : null;
-$nombres = isset($_POST["name"]) ? mb_strtoupper(trim($_POST["name"]), 'UTF-8') : null;
-$apellidos = isset($_POST["apellidos"]) ? mb_strtoupper(trim($_POST["apellidos"]), 'UTF-8') : null;
-$direccion = isset($_POST["dire"]) ? mb_strtoupper(trim($_POST["dire"]), 'UTF-8') : null;
+$nombres = isset($_POST["nombre"]) ? mb_strtoupper(trim($_POST["name"]), 'UTF-8') : null;
+$apellidos = isset($_POST["apellidoos"]) ? mb_strtoupper(trim($_POST["apellidos"]), 'UTF-8') : null;
+$direccion = isset($_POST["direccion"]) ? mb_strtoupper(trim($_POST["dire"]), 'UTF-8') : null;
 $telefono = isset($_POST["telefono"]) ? trim($_POST["telefono"]): null;
-$correo = isset($_POST["mail"]) ? trim($_POST["mail"]): null;
-$fechaNacimiento = isset($_POST["fecha"]) ? trim($_POST["fecha"]): null;
-$contrasena = isset($_POST["contra"]) ? trim($_POST["contra"]) : null;
+$ciudad = isset($_POST["ciudad"]) ? trim($_POST["ciudad"]): null;
+$provincia = isset($_POST["provincia"]) ? trim($_POST["provincia"]): null;
+$correo = isset($_POST["correo"]) ? trim($_POST["correo"]): null;
+$contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
 $rol=isset($_POST["rol"]) ? trim($_POST["rol"]): 'U'; 
 $sql = "INSERT INTO usuario VALUES (0, '$cedula', '$nombres', '$apellidos', '$direccion', '$telefono',
 '$correo','$contrasena', '$fechaNacimiento', 'N', null, null ,'$rol')";
