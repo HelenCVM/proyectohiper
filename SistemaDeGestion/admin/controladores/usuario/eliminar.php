632d390b5b7067bb541fbd5a2bd5af046b6da68a
HelenCVM <52894597+HelenCVM@users.noreply.github.com>
2019-12-02 15:49:07 -0500

    EliminandoArchivos

--- SistemaDeGestion/admin/controladores/usuario/eliminar.php
+++ SistemaDeGestion/admin/controladores/usuario/eliminar.php
@@ -13,10 +13,10 @@
     date_default_timezone_set("America/Guayaquil");     
     $fecha = date('Y-m-d H:i:s', time());    
     $sql = "DELETE FROM Usuario WHERE usu_codigo = '$codigo'";  
- 
     if ($conn->query($sql) === TRUE) {                 
         echo "<p>Se ha eliminado los datos correctamemte!!!</p>";
-                  
+        header("Location:../../../public/vista/login.php");  
+
     } else {                 
         echo "<p>Error: " . $sql . "<br>" . mysqli_error($conn) . "</p>";             
     }     echo "<a href='../../vista/usuario/index.php'>Regresar</a>"; 
