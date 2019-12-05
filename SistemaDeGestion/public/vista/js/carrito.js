0c1adccdcffce5f4414ea77f8335441b00e12f06
dguzmanc4 <dguzmanc4@est.ups.edu.ec>
2019-12-05 17:08:59 -0500

    Carrito

--- SistemaDeGestion/public/vista/js/carrito.js
+++ SistemaDeGestion/public/vista/js/carrito.js
@@ -23,3 +23,36 @@ function cartAdd(cod) {
     xmlhttp.open("GET", "../controladores/cartAdd.php?codProd=" + cod , true)
     xmlhttp.send()
 }
+
+
+function cluseWindowCart() {
+    let windowFloat = document.getElementById("cartAdd")
+    windowFloat.style.display = "none"
+}
+
+function factura(usu,subtotal,iva,total){
+    console.log(usu);
+    console.log(subtotal);
+    console.log(iva);
+    console.log(total);
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest()
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            
+            if (document.getElementById("cartAdd") === null) {
+                document.body.innerHTML += this.responseText
+            } else {
+                openWindowCart()
+            }
+            //document.getElementById('body').innerHTML = this.responseText
+        }
+    };
+    xmlhttp.open("GET", "../controladores/factura.php?codUsu=" + usu+ "&subt=" + subtotal+"&iva=" + iva+"&total=" + total)
+    console.log("../controladores/factura.php?codUsu=" + usu+ "&subt=" + subtotal+"&iva=" + iva+"&total=" + total);
+    xmlhttp.send()
+
+}
