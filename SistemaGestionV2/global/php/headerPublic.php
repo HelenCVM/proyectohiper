f4aef5df9c6979380ba062fbed4cb7b749422953
Edw1nM4rquez <edwi1999@hotmail.com>
2019-12-06 09:06:31 -0500

    actualizamos menu

--- SistemaGestionV2/global/php/headerPublic.php
+++ SistemaGestionV2/global/php/headerPublic.php
@@ -1,10 +1,13 @@
 <script src="../js/funciones.js"></script>
+<center><a href="index.php" ><img src="../../img/banner-imi.png"  alt="Import Mangueras"/></a></center>
+
 <div class="content">
-    <a href="../../index.php"></a>
+    <a href="../../index.php"></a>   
     <nav class="menu">
         <ul>
-            <li><a href="../../index.php">Inicio</a></li>
-            <li> <span>Categorias</span> <i class="fas fa-sort-down"></i>
+            <li><a href="../../index.php"><img id ="iconmenu" src="../../img/icon1.png">INICIO</a></li>
+            <li><a href="nosotros.php"><img id ="iconmenu" src="../../img/icon2.png"> NOSOTROS</a></li>
+            <li> <span><img id ="iconmenu" src="../../img/icon3.png"> PRODUCTOS</span> <i class="fas fa-sort-down"></i>
                 <ul>
                     <li><a href="categoria.php?categoria=10">Industriales</a></li>
                     <li><a href="categoria.php?categoria=11">Hidraulicas</a></li>
@@ -12,17 +15,13 @@
                 </ul>
             </li>
             <li><span>Mision y Vision </span></li>
-<li><span>¿Quienes somo?</span></li>
-<li><span>Contactanos</span></li>
+            <li><a href="contacto.php"><img id ="iconmenu" src="../../img/icon4.png"> CONTACTOS </a></li>
+                <li><a href="login.php"><img id ="iconmenu" src="../../img/icon5.png"> LOGIN</a></li>                
+                <li><a href="formulario.php"> <img id ="iconmenu" src="../../img/icon6.png"> REGISTRATE</a></li>     
+                
         </ul>
     </nav>
-    <div class="search">
-        <div class="barSearch">
-            <input type="search" name="search" id="search" placeholder="Buscar" onkeyup="searchBox(this)">
-            <i class="fas fa-search"></i>
-        </div>
-        <a onclick="searchBtn('search.php')">Buscar</a>
-    </div>
+   
     <!-- <div class="buyCar itemsUser">
         <a href="../../public/view/shoppingcart.php"><i class="fas fa-shopping-cart"></i></a>
     </div> -->
