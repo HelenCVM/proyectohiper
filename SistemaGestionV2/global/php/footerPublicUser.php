d9aee7ba24d936590dbdf35a5c56a581d3e690bb
dguzmanc4 <dguzmanc4@est.ups.edu.ec>
2019-12-06 09:32:30 -0500

    Sesiones

--- SistemaGestionV2/global/php/footerPublicUser.php
+++ SistemaGestionV2/global/php/footerPublicUser.php
@@ -12,24 +12,7 @@
 
         </ul>
     </div>
-    <!--<div class="listFooter">
-        <h3>Tiendas</h3>
-        <ul>
-            <li><a href="#">Quito</a></li>
-            <li><a href="#">Guayaquil</a></li>
-            <li><a href="#">Cuenca</a></li>
-        </ul>
-    </div>-->
-    <div class="listFooter">
-        <h3>Desarrolladores</h3>
-        <ul>
-            <li><a href="#">Henry Guaman</a></li>
-            <li><a href="#">Claudio Maldonado</a></li>
-            <li><a href="#">Jonnathan Ochoa</a></li>
-            <li><a href="#">Pedro Ortiz</a></li>
-            <li><a href="#">Yandry Romero</a></li>
-        </ul>
-    </div>
+
     <div class="infoFooter">
         <p>Copyright &copy; 2019 Todos los derechos reservados</p>
         <p>Designed by Group 5</p>
